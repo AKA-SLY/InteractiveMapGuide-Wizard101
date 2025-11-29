83b46322d6640b5b786db6f34dc793e0ade1cea5
Sly <ampsmods@gmail.com>
2025-11-29 13:53:55 -0500

    Add files via upload

---
+++ wizard101-guide/src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Wizard101Preview from "./Wizard101Preview";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Wizard101Preview />
+  </React.StrictMode>
+);
