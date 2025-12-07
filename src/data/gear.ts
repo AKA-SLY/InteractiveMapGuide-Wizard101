67cd7796d5961ea53d2e300ad59bce53be71369f
Sly <ampsmods@gmail.com>
2025-12-06 23:43:05 -0500

    Clean layout for organised branch

---
+++ src/data/gear.ts
@@ -0,0 +1,28 @@
+import { type Gear } from "../types";
+
+export const gear: Gear[] = [
+  {
+    name: "Novice Fire Hat",
+    school: "Fire",
+    type: "Hat",
+    level: 10,
+    stats: "+5% Accuracy, +40 Health",
+    location: "Vendor — The Commons",
+  },
+  {
+    name: "Ice Seafarer Robe",
+    school: "Ice",
+    type: "Robe",
+    level: 15,
+    stats: "+75 Health, +8% Resist",
+    location: "Drop — Lady Blackhope (Wizard City)",
+  },
+  {
+    name: "Shocking Blade",
+    school: "Storm",
+    type: "Athame",
+    level: 20,
+    stats: "+75 Health, +7% Damage",
+    location: "Crafted — Marleybone",
+  },
+];
